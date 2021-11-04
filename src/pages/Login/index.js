c7b9a896445a9348d6cfe0838626a5b76a747d25
Felype Henrique <felypehenrique25@gmail.com>
2021-11-04 15:01:46 -0300

    Commit inicial

---
+++ src/pages/Login/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Text} from 'react-native' 
+
+import {Container} from '../../styles/index'
+
+const Login = () => {
+    return (
+        <Container>
+            <Text>Login</Text>
+        </Container>
+    );
+};
+
+export default Login;
