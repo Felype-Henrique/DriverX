5c4989c9f9d2bdcbc76ac59a91cb56ede7abc071
Felype Henrique <felypehenrique25@gmail.com>
2021-11-09 15:35:28 -0300

    Rotas com React Navigation

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Routes from './src/routes';
 import {Provider} from 'react-redux';
 import store from './src/store'
 
@@ -9,14 +10,17 @@ import Car from './src/pages/Steps/car';
 import Payment from './src/pages/Steps/payment';
 import Ride from './src/pages/Ride/index'
 import Home from './src/pages/Home';
+
 import { StatusBar } from 'react-native';
 import { Container } from './src/styles';
+
+
 
 export default function App() {
   return (
     <Provider store={store}>
       <StatusBar/>
-      <Home/>
+      <Routes/>
     </Provider>
       
   );
